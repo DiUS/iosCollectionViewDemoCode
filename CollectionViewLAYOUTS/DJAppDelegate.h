9b79992e11d685780f3bfc5ac87ea6c0974d2626
Dallas Johnson <dallasj001@gmail.com>
2014-02-02 00:16:51 +1100

    Initial Commit

---
+++ CollectionViewLAYOUTS/DJAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  DJAppDelegate.h
+//  CollectionViewLAYOUTS
+//
+//  Created by Dallas Johnson on 2/02/2014.
+//  Copyright (c) 2014 Dallasj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DJAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ CollectionViewLAYOUTS/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  CollectionViewLAYOUTS
+//
+//  Created by Dallas Johnson on 2/02/2014.
+//  Copyright (c) 2014 Dallasj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "DJAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+  @autoreleasepool {
+      return UIApplicationMain(argc, argv, nil, NSStringFromClass([DJAppDelegate class]));
+  }
+}
